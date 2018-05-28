e6cd2e59971d8a7de98166a981e7135026071815
scyclow <spike716@gmail.com>
2018-05-28 01:09:43 -0400

    [wip] caching, distriibutions, tests

--- contracts/NotASecurity.sol
+++ contracts/NotASecurity.sol
@@ -10,18 +10,24 @@ contract NotASecurity {
   mapping (address => uint) public balanceOf;
   mapping (address => mapping (address => uint)) internal allowed;
 
-  address[10] public benefactors;
+  address[11] public benefactors;
   uint public benefactorsBalance;
+
+  // Caching things for performance reasons
+  mapping (address => uint8) private benefactorMap;
+  address private lowestBenefactor;
 
   event Approval(address indexed _owner, address indexed _spender, uint _value);
   event Transfer(address indexed _from, address indexed _to, uint _value);
 
   constructor() public {
     uint _fee = 10;
-    benefactors[0] = msg.sender;
+    benefactors[1] = msg.sender;
+    lowestBenefactor = 0x0;
+    benefactorMap[msg.sender] = 1;
     balanceOf[msg.sender] = _fee;
     totalSupply = _fee;
-    benefactorBalance = _fee;
+    benefactorsBalance = _fee;
   }
 
   function buy() payable public returns (uint) {
@@ -29,11 +35,11 @@ contract NotASecurity {
     address _investor = msg.sender;
 
     require(_wei > 0);
+    require(distribute(_wei));
 
     balanceOf[_investor] += _wei;
     totalSupply += _wei;
 
-    require(distribute(_wei));
     require(reorganize(_wei, _investor));
 
     return _wei;
@@ -43,13 +49,17 @@ contract NotASecurity {
     buy();
   }
 
-  function distribute(uint _amount) private returns (bool) {
-    for (uint _i = 0; _i < benefactors.length; _i++) {
+  event Distribution(address _addr, uint _amount);
+
+  function distribute(uint _amount) public returns (bool) {
+    for (uint _i = 1; _i < benefactors.length; _i++) {
       address _benefactor = benefactors[_i];
       uint _benefactorBalance = balanceOf[_benefactor];
-      uint _amountToTransfer = (_benefactorBalance * _amount) / (benefactorsBalance * _amount);
 
-      if (_benefactor != address(0)) {
+      uint _amountToTransfer = (_benefactorBalance * _amount) / benefactorsBalance;
+      emit Distribution(_benefactor, _amountToTransfer);
+
+      if (_amountToTransfer > 0 && _benefactor != address(0)) {
         _benefactor.transfer(_amountToTransfer);
       }
     }
@@ -57,14 +67,59 @@ contract NotASecurity {
     return true;
   }
 
-  function reorganize(uint _amount, address _investor) private returns (bool) {
-    for (uint _i = 0; _i < benefactors.length; _i++) {
+  function findLowestBenefactor() public returns (address) {
+    address _lowestBenefactor = benefactors[1];
+    address _benefactor;
+    for (
+      uint _j = 2;
+      _j < benefactors.length;
+      _j++
+    ) {
+      _benefactor = benefactors[_j];
+      if (_benefactor == address(0)) {
+        return _benefactor;
+
+      } else if (balanceOf[_benefactor] < balanceOf[_lowestBenefactor]) {
+        _lowestBenefactor = _benefactor;
+      }
+    }
+    return _lowestBenefactor;
+  }
+
+  function findEmptyBenefactorIndex() public returns (uint8) {
+    for (uint8 _i = 1; _i < benefactors.length; _i++) {
+      if (benefactors[_i] == address(0)) {
+        return _i;
+      }
     }
 
-    // if _investor's balance is greater than the lowest benefactor's balance, hold onto both
-    // if "" swap out lowest benefactor for investor
-    // adjustment = balanceOf[newBenefactor] - balanceOf[benefactorToRemove]
-    // handle case where benefactor buys more
+    return 0;
+  }
+
+  event Log(address addr, string str);
+  function reorganize(uint _amount, address _investor) public returns (bool) {
+    // if investor is already a benefactor
+    if (benefactorMap[_investor] > 0) {
+      benefactorsBalance += _amount;
+
+    // if investor is now a top token holder
+    } else if (balanceOf[_investor] > balanceOf[lowestBenefactor]) {
+      uint _oldBalance = balanceOf[lowestBenefactor];
+      uint8 _indexToSwap = benefactorMap[lowestBenefactor];
+
+      // Swap out benefactors
+      benefactorMap[lowestBenefactor] = 0;
+      benefactors[_indexToSwap] = _investor;
+      benefactorMap[_investor] = _indexToSwap;
+      emit Log(_investor, 'investor');
+      emit Log(lowestBenefactor, 'lowestBenefactor');
+      lowestBenefactor = findLowestBenefactor();
+      emit Log(lowestBenefactor, 'new lowestBenefactor');
+
+      // Adjust benefactors balance
+      benefactorsBalance += (balanceOf[_investor] - _oldBalance);
+    }
+
     return true;
   }
 
--- test/tests.js
+++ test/tests.js
@@ -11,74 +11,116 @@ contract('NotASecurity', (accounts) => {
     const decimals = await Contract.decimals()
     const symbol = await Contract.symbol()
     const name = await Contract.name()
-    const totalSupply = (await Contract.totalSupply()).toNumber()
-    const balance = (await Contract.balanceOf(alice)).toNumber()
+    const totalSupply = (await Contract.totalSupply())
+    const balance = (await Contract.balanceOf(alice))
 
     expect(decimals.toNumber()).to.equal(18)
     expect(symbol).to.equal('NOS')
     expect(name).to.equal('NotASecurity')
-    expect(totalSupply).to.equal(balance)
+    assert(totalSupply.equals(balance))
     expect(totalSupply > 0).to.equal(true)
   })
 
-  it('should allow investors to buy buy NOS', async () => {
-    const aliceBalance0 = (await Contract.balanceOf(alice)).toNumber()
+  it('should allow investors to buy NOS', async () => {
+    const aliceBalance0 = (await Contract.balanceOf(alice))
     const amountToBuy = 50
     await Contract.buy({ from: alice, value: amountToBuy })
     await Contract.sendTransaction({ from: bob, value: amountToBuy })
 
-    const totalSupply = (await Contract.totalSupply()).toNumber()
-    const aliceBalance = (await Contract.balanceOf(alice)).toNumber()
-    const bobBalance = (await Contract.balanceOf(bob)).toNumber()
+    const totalSupply = (await Contract.totalSupply())
+    const aliceBalance = (await Contract.balanceOf(alice))
+    const bobBalance = (await Contract.balanceOf(bob))
 
-    expect(totalSupply).to.equal(aliceBalance0 + amountToBuy + amountToBuy)
-    expect(aliceBalance - aliceBalance0).to.equal(amountToBuy)
-    expect(bobBalance).to.equal(amountToBuy)
+    const expectedSupply = aliceBalance0.plus(amountToBuy).plus(amountToBuy)
+    const expectedBalance = aliceBalance0.plus(amountToBuy)
+
+    assert(totalSupply.equals(expectedSupply))
+    assert(aliceBalance.equals(expectedBalance))
+    assert(bobBalance.equals(amountToBuy))
   });
 
   it('should allow investors to transfer NOS', async () => {
-    const aliceBalance0 = (await Contract.balanceOf(alice)).toNumber()
+    const aliceBalance0 = (await Contract.balanceOf(alice))
     const amountToBuy = 50
     await Contract.buy({ from: alice, value: amountToBuy })
     const transfer = await Contract.transfer(bob, amountToBuy, { from: alice })
 
-    const totalSupply = (await Contract.totalSupply()).toNumber()
-    const aliceBalance = (await Contract.balanceOf(alice)).toNumber()
-    const bobBalance = (await Contract.balanceOf(bob)).toNumber()
+    const totalSupply = (await Contract.totalSupply())
+    const aliceBalance = (await Contract.balanceOf(alice))
+    const bobBalance = (await Contract.balanceOf(bob))
 
-    expect(totalSupply).to.equal(aliceBalance0 + amountToBuy)
-    expect(aliceBalance).to.equal(aliceBalance0)
-    expect(bobBalance).to.equal(amountToBuy)
+    assert(totalSupply.equals(aliceBalance0.plus(amountToBuy)))
+    assert(aliceBalance.equals(aliceBalance0))
+    assert(bobBalance.equals(amountToBuy))
 
     expect(transfer.logs[0].event).to.equal('Transfer')
-
   })
 
   it('should handle allowances properly', async () => {
-    const aliceBalance0 = (await Contract.balanceOf(alice)).toNumber()
-    const totalSupply0 = (await Contract.totalSupply()).toNumber()
+    const aliceBalance0 = await Contract.balanceOf(alice)
+    const totalSupply0 = await Contract.totalSupply()
 
     // Approval
     const approval = await Contract.approve(bob, aliceBalance0)
-    const allowance1 = (await Contract.allowance(alice, bob)).toNumber()
-    const totalSupply1 = (await Contract.totalSupply()).toNumber()
+    const allowance1 = await Contract.allowance(alice, bob)
+    const totalSupply1 = await Contract.totalSupply()
 
-    expect(allowance1).to.equal(aliceBalance0)
-    expect(totalSupply1).to.equal(totalSupply0)
+    assert(allowance1.equals(aliceBalance0))
+    assert(totalSupply1.equals(totalSupply0))
 
     // Transfer
-    const transfer = await Contract.transferFrom(alice, charlie, allowance1, { from: bob })
-    const allowance2 = (await Contract.allowance(alice, bob)).toNumber()
-    const totalSupply2 = (await Contract.totalSupply()).toNumber()
-    const aliceBalance2 = (await Contract.balanceOf(alice)).toNumber()
-    const charlieBalance2 = (await Contract.balanceOf(charlie)).toNumber()
+    const failedTransfer = await Contract.transferFrom(alice, charlie, allowance1, { from: diane })
+      .catch(() => false)
+    expect(failedTransfer).to.equal(false)
 
-    expect(charlieBalance2).to.equal(aliceBalance0)
-    expect(aliceBalance2).to.equal(0)
-    expect(allowance2).to.equal(0)
-    expect(totalSupply2).to.equal(totalSupply0)
+    const transfer = await Contract.transferFrom(alice, charlie, allowance1, { from: bob })
+    const allowance2 = await Contract.allowance(alice, bob)
+    const totalSupply2 = await Contract.totalSupply()
+    const aliceBalance2 = await Contract.balanceOf(alice)
+    const charlieBalance2 = await Contract.balanceOf(charlie)
+
+    assert(charlieBalance2.equals(aliceBalance0))
+    assert(aliceBalance2.equals(0))
+    assert(allowance2.equals(0))
+    assert(totalSupply2.equals(totalSupply0))
+
+    const failedTransfer2 = await Contract.transferFrom(alice, charlie, allowance1, { from: bob })
+      .catch(() => false)
+    expect(failedTransfer2).to.equal(false)
 
     expect(transfer.logs[0].event).to.equal('Transfer')
     expect(approval.logs[0].event).to.equal('Approval')
+  })
+
+  it.only('should redistribute incoming ethereum according to tokenholder ratios', async () => {
+    const aliceEthBalance0 = web3.eth.getBalance(alice)
+    const bobEthBalance0 = web3.eth.getBalance(bob)
+    const amountToBuy = (await Contract.balanceOf(alice)).times(2)
+
+    const receipt = await Contract.buy({ from: bob, value: amountToBuy })
+    const gasUsed = receipt.receipt.gasUsed;
+    const tx = await web3.eth.getTransaction(receipt.tx);
+    const {gasPrice} = tx;
+
+    const gasCost = gasPrice * gasUsed
+    const aliceEthBalance1 = web3.eth.getBalance(alice)
+    const bobEthBalance1 = web3.eth.getBalance(bob)
+    const charlieEthBalance1 = web3.eth.getBalance(charlie)
+
+    assert(aliceEthBalance1.equals(aliceEthBalance0.plus(amountToBuy)), '1')
+    assert(bobEthBalance1.equals(bobEthBalance0.minus(amountToBuy).minus(gasCost)), `${bobEthBalance1}, ${bobEthBalance0}, ${amountToBuy}`)
+
+    await Contract.buy({ from: charlie, value: 3 })
+
+    const aliceEthBalance2 = web3.eth.getBalance(alice)
+    const bobEthBalance2 = web3.eth.getBalance(bob)
+    const charlieEthBalance2 = web3.eth.getBalance(charlie)
+
+    assert(aliceEthBalance2.equals(aliceEthBalance1.plus(1)), `${aliceEthBalance2.toNumber()}, ${aliceEthBalance1.plus(1).toNumber()}`)
+    for(let i = 1; i < 11; i++) {
+      console.log(await Contract.benefactors(i))
+    }
+    assert(bobEthBalance2.equals(bobEthBalance1.plus(2)), `${bobEthBalance2}, ${bobEthBalance1}`)
+    assert(charlieEthBalance2.equals(charlieEthBalance1.minus(3).minus(gasCost)), '5')
   })
 })
